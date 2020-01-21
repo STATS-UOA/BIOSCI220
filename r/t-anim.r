994d59070e5e65341ae951e3a132b687db6b45e0
cmjt <cmjonestodd@gmail.com>
2020-01-22 06:30:43 +1300

    updating randomisation and anova plots

--- r/t-anim.r
+++ r/t-anim.r
@@ -3,166 +3,57 @@ library(ggplot2)
 library(gganimate)
 library(magick)
 
-## One sample t-test
-## simulate data
-dog <- data.frame(type = "Food",weight = rnorm(100,5,1), reps = rep(1:10,each = 10))
+study <- round(runif(10,80,100))
+no_study <- round(runif(10,40,90))
 
-means_df <- dog %>%
-    group_by(reps,type) %>%
-    summarize(means = mean(weight),
-              sem = sd(weight)/sqrt(length(weight)))
+study_df<-data.frame(student=seq(1:10),study,no_study)
+mean_original<-data.frame(IV=c("studied","did not study"),
+                          means=c(mean(study),mean(no_study)))
+t_df <- data.frame(sims=rep(1,20),
+                 IV=rep(c("studied","did not study"),each=10),
+                 values=c(study,no_study),
+                 rand_order=rep(c(0,1),each=10))
 
-stats_df <- dog %>%
-    group_by(reps) %>%
-    summarize(ts = t.test(weight,mu = 5)$statistic)
-
-
-a <- ggplot(means_df, aes(x = type, y = means)) +
-    geom_bar(stat = "identity") +
-    geom_point(aes(x = type, y = weight), data = dog, alpha = .25) +
-    geom_errorbar(aes(ymin = means - sem, ymax = means + sem), width = .2) +
-    theme(axis.title.x=element_blank(),
-          axis.ticks.x=element_blank(),
-          panel.background = element_blank(),
-          panel.grid.minor = element_blank(),
-          panel.grid.major = element_blank(),
-          panel.border = element_rect(colour='black', fill=NA)) +
-    geom_hline(yintercept = 5, col = "red") + 
-    xlab("") + ylab("Weight of dog food (kg)") +
-    transition_states(
-        states = reps,
-        transition_length = 2,
-        state_length = 1) +
-    geom_text(aes(0.5, 7.5,label = paste("Mean:", format(round(means, 3),"kg", nsmall = 2))),
-              size = 4, hjust = 0, color = "black") +
-    enter_fade() + 
-    exit_shrink() +
-    ease_aes('sine-in-out')
-  
-a_gif <- animate(a, width = 420, height = 420)
-
-b <- ggplot(stats_df, aes(x = ts))+
-  geom_vline(aes(xintercept = ts, frame = reps))+
-  geom_line(aes(x=x,y=y),
-            data = data.frame(x = seq(-5,5, .1),
-                              y = dt(seq(-5,5, .1), df = 18))) +
-    theme(panel.background = element_blank(),
-          panel.grid.minor = element_blank(),
-          panel.grid.major = element_blank(),
-          panel.border = element_rect(colour='black', fill=NA)) +
-  ylab("density") +
-  xlab("t statistic") +
-  transition_states(
-    states = reps,
-    transition_length = 2,
-    state_length = 1
-  ) +
-    geom_text(aes(0.5, 7.5,label = paste("t-statistic:", format(round(ts, 2), nsmall = 2))),
-              size = 4, hjust = 0, color = "black") +
-  enter_fade() + 
-  exit_shrink() +
-  ease_aes('sine-in-out')
-
-b_gif <- animate(b, width = 420, height = 420)
-
-a_mgif <- image_read(a_gif)
-b_mgif <- image_read(b_gif)
-
-new_gif <- image_append(c(a_mgif[1], b_mgif[1]))
-for(i in 2:100){
-  combined <- image_append(c(a_mgif[i], b_mgif[i]))
-  new_gif <- c(new_gif, combined)
+raw_df<-t_df
+for(i in 2:10){
+  new_index<-sample(1:20)
+  t_df$values<-t_df$values[new_index]
+  t_df$rand_order<-t_df$rand_order[new_index]
+  t_df$sims<-rep(i,20)
+  raw_df<-rbind(raw_df,t_df)
 }
 
-new_gif
+raw_df$rand_order<-as.factor(raw_df$rand_order)
+rand_df<-aggregate(values~sims*IV,raw_df,mean)
+names(rand_df)<-c("sims","IV","means")
+rand_df$rand_order <- as.factor(1)
 
-## independant t-test
-## simulate data
-A <-rnorm(100,50,10)
-B <-rnorm(100,45,10)
-DV <- c(A,B)
-Student <- rep(c("Medics","Biologists"),each=100)
-sims <- rep(rep(1:10,each=10),2)
-df <- data.frame(sims, Student, DV)
 
-means_df <- df %>%
-               group_by(sims,Student) %>%
-               summarize(means=mean(DV),
-                         sem = sd(DV)/sqrt(length(DV)))
-means_df$lab <- rep(1:2, times = 10)
-
-stats_df <- df %>%
-              group_by(sims) %>%
-              summarize(ts = t.test(DV~Student,var.equal=TRUE)$statistic)
-
-a <- ggplot(means_df, aes(x=Student,y=means, fill=Student))+
-    geom_bar(stat="identity")+
-    geom_point(data=df,aes(x=Student, y=DV), alpha=.25)+
-    geom_errorbar(aes(ymin=means-sem, ymax=means+sem),width=.2) +
-    ylab("Number of hours studied") +
-    theme(axis.title.x=element_blank(),
+a <- ggplot(data = raw_df,aes(x=IV,y=values,color=rand_order), color = c("red","blue")) +
+    geom_point(stat="identity",alpha=.5,size = 6) +
+    geom_point(data=mean_original,aes(x = IV,y = means,),
+               stat="identity",shape=21,size = 6,color="black",fill="black") +
+    geom_point(data=rand_df,aes(x=IV,y=means),stat="identity",
+               shape=21,size=6,color="gold",fill="gold") +
+    geom_text(data=rand_df,aes(x = IV,y=means,label = "Randomized mean"),stat="identity", color = "black",
+              nudge_x = -0.3) +
+    coord_cartesian(ylim=c(40, 100)) +
+    ylab("Exam grade") +
+    scale_color_manual(values = c("red","blue"),
+                       labels = c("studied","did not study"),name = "Original data") +
+    annotate("text",x = c(1.25,2.25), y = rev(mean_original$means), label = "Original mean") + 
+    theme(legend.position="bottom",
+          axis.title.x=element_blank(),
           panel.background = element_blank(),
           panel.grid.minor = element_blank(),
           panel.grid.major = element_blank(),
           panel.border = element_rect(colour='black', fill=NA)) +
     transition_states(
-        states = sims,
-        transition_length = 2,
-        state_length = 1
-    ) +
-    geom_text(aes(0.5, 80,label = paste("Sample mean Biologists:",
-                                        format(round(means[c(sims[c(TRUE,TRUE,FALSE,FALSE)],
-                                                             means_df$sims[c(TRUE,TRUE,FALSE,FALSE)] + 10)], 3),
-                                               nsmall = 2))),
-              size = 4, hjust = 0, color = "black") +
-    geom_text(aes(0.5, 76,label = paste("Sample mean Medics:", format(round(means[sims*2], 3),
-                                                                      nsmall = 2))),
-              size = 4, hjust = 0, color = "black") +
-    enter_fade() + 
+        sims,
+        transition_length = 1,
+        state_length = 2
+    ) + enter_fade() +
     exit_shrink() +
     ease_aes('sine-in-out')
 
-a_gif <- animate(a, width = 420, height = 420)
-
-b <- ggplot(stats_df,aes(x=ts))+
-    geom_vline(aes(xintercept=ts, frame=sims))+
-    geom_line(data=data.frame(x=seq(-5,5,.1),
-                              y=dt(seq(-5,5,.1),df=18)),
-              aes(x=x,y=y)) +
-    theme(panel.background = element_blank(),
-          panel.grid.minor = element_blank(),
-          panel.grid.major = element_blank(),
-          panel.border = element_rect(colour='black', fill=NA)) +
-    ylab("density")+
-    xlab("t-statistic")+
-    transition_states(
-        states=sims,
-        transition_length = 2,
-        state_length = 1
-    ) +
-    geom_text(aes(-4, 0.5,label = paste("t-statistic:", format(round(ts, 2), nsmall = 2))),
-              size = 4, hjust = 0, color = "black") +
-    enter_fade() + 
-    exit_shrink() +
-    ease_aes('sine-in-out')
-
-b_gif<-animate(b, width = 420, height = 420)
-
-
-d<-image_blank(420*2,420)
-
-the_frame<-d
-for(i in 2:100){
-  the_frame<-c(the_frame,d)
-}
-
-a_mgif<-image_read(a_gif)
-b_mgif<-image_read(b_gif)
-
-new_gif<-image_append(c(a_mgif[1], b_mgif[1]))
-for(i in 2:100){
-  combined <- image_append(c(a_mgif[i], b_mgif[i]))
-  new_gif<-c(new_gif,combined)
-}
-
-new_gif
+animate(a,nframes=100,fps=5)
