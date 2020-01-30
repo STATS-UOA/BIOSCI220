6afb35eba27dc250c97fc7d21616768429133946
cmjt <cmjt@st-andrews.ac.uk>
2020-01-30 14:06:18 +1300

    theme

--- r/t-anim.r
+++ r/t-anim.r
@@ -1,6 +1,24 @@
+library(ggplot2)
 
 
+alpha <- function(x,mean,sd) {
+    y <- dnorm(x, mean = mean, sd = sd)
+    y[x >= (mean - 2 * sd) & x <= (mean + 2 * sd)] <- NA
+    return(y)
+}
+beta <- function(x,cv,mean,sd){
+    y <- dnorm(x, mean = mean, sd = sd)
+    y[x > cv] <- NA
+    return(y)
+}
 
+plt <- ggplot(data.frame(x = c(-10,25, 1)), aes(x = x)) +
+        stat_function(fun = dnorm, args = list(0, 3), size = 1.5) +
+    stat_function(fun = dnorm, args = list(10, 3), size = 1.5) + 
+    theme_void() + stat_function(fun=alpha, geom="area", fill="#84CA72", alpha=0.2,args = list(0, 3)) +
+    stat_function(fun=beta, geom="area", fill="red", alpha=0.2,args = list(6,10,3)) + geom_vline(xintercept = 6)
+
+plt
 
 
 
